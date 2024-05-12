c855fc7420e8114efe9ce3c9a399c83d967eebf8
Marko Lähde <marko.lahde@helsinki.fi>
2024-05-12 21:37:00 +0300

    Couple fixes to be able to run this on Docker

--- secprojectI/flawsapp/models.py
+++ secprojectI/flawsapp/models.py
@@ -8,7 +8,7 @@ import uuid
 class URLNotes(models.Model):
 	# A01:2021 – Broken Access Control
 	# on_delete=models.CASCADE should be added to ForeingKey definition below
-	user = models.ForeignKey(User)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, auto_created = True)
 	notes = models.TextField()
 	url = models.URLField()
